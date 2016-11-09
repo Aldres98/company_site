e0f6483a994c169c2a64aa7acf37bf979090d15b
Aldres98 <aldres98@gmail.com>
2016-11-09 11:14:10 +0300

    seed db

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :set_project, only: [:show, :index]
 
   # GET /projects
   # GET /projects.json
@@ -11,7 +11,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
